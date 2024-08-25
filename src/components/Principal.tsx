ad5ee4b6204cdab1cd387b64b3133ab34c6dab91
Felipe <juanf.lopezs@autonoma.edu.co>
2024-08-25 16:59:19 -0500

    Creacion de proyecto y reestructuracion

---
+++ src/components/Principal.tsx
@@ -0,0 +1,12 @@
+export default function Principal() {
+  return (
+    <main className="main">
+        <aside className="aside">
+            This is the aside
+        </aside>
+        <section className="principal">
+            This is the principal content
+        </section>
+    </main>
+  )
+}
